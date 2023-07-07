439e14681a410689693b719b67c16ba022efbaa8
ahmedraza90 <34ahmed.raza@gmail.com>
2023-07-07 19:45:55 +0500

    animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style.css';
 import helmet from './images/FullImage.png'
 import logo from './images/LOGO.png'
+import twitter from './images/twitter.png'
 // import './styl.css';
 
 
@@ -10,62 +11,55 @@ function App() {
 
   return (
     <div>
-      {/* <center>
-          <h2>
-            MOWS,&nbsp;&nbsp;&nbsp;inc .
-          </h2>
-        </center>
-        <center>
+      <center>
+        <h2>
+          MOWS,&nbsp;&nbsp;&nbsp;inc .
+        </h2>
+      </center>
+      <center>
         <div className="Logo">
           <img src={logo} alt="" />
         </div>
-        </center> */}
-      <div className="parent">
+      </center>
+      <center>
+        <h1 className="Heading">
+          A PARADIGM SHIFT BEGINS
+        </h1>
+      </center>
+      <div>
         <center>
           <div className="helmet">
             <img src={helmet} alt="" />
           </div>
         </center>
-
-        
-
-        {/* <div className="helmet">
-          <img src={helmet} alt="" />
-        </div>
-        <div className="logoText">
-          <center>
-            MOWS,&nbsp;&nbsp;&nbsp;inc .
-          </center>
-        </div>
-        <div className="Logo">
-          <img src={logo} alt="" />
-        </div>
-        <div className="Heading">
-          A PARADIGM SHIFT BEGINS
-        </div>
-        <div className='lines'>
-          <div className='box1'>
-            helmet
-          </div>
-          <div className='box2'>
-            visor
-          </div>
-          <div className='box3'>
-            neck
-          </div>
-        </div>
-        <div className='bottomText'>
-          <div className="text1">
-            An NFT Company
-          </div>
-          <div className="text2">
-            WITH FIVE YEARS OF EXPERIENCE
-          </div>
-          <div className="text3">
-            LANUCHING FREE MINTS FOR TWITTER MFERS
-          </div>
-        </div> */}
       </div>
+      <center>
+        <div >
+          <p className='text-color'>
+            An NFT Company
+          </p>
+          <p style={{ color: "white" }}>
+            WITH FIVE YEARS OF EXPERIENCE
+          </p>
+          <p style={{ color: "white" }}>
+            LANUCHING FREE MINTS FOR TWITTER MFERS
+          </p>
+        </div>
+      <center>
+        <div style={{ marginBlock:"10px", marginTop:"20px" }} onClick={() =>
+                  window.open('https://twitter.com/oasisx_world', '_blank')
+                }>
+            < img src={twitter} alt="" style={{ height: "30px", width:"30px" }}></img>
+        </div>
+      </center>
+      </center>
+      <center>
+        <div>
+          <p className='text-color'>
+            All Rights Reserved
+          </p>
+        </div>
+      </center>
     </div>
   );
 }
